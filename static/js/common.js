866ab331c1cda6cc29a0def5ac4db6cdbec41dc6
starsriver <starsriver@yahoo.com>
2020-07-21 16:43:53 +0800

    界面交互优化

--- static/js/common.js
+++ static/js/common.js
@@ -109,7 +109,7 @@ function in_array(needle, haystack) {
 	if(typeof needle == 'string' || typeof needle == 'number') {
 		for(var i in haystack) {
 			if(haystack[i] == needle) {
-					return true;
+			    return true;
 			}
 		}
 	}
---
+++ static/js/common_protos.js
@@ -0,0 +1,127 @@
+Object.defineProperty(HTMLElement.prototype, 'Css', {
+    get: function () {
+        var self = this;
+
+        function calcstyle(sty) {
+            if (self.currentStyle) {
+                return self.currentStyle[sty];
+            } else {
+                return window.getComputedStyle(self, null)[sty];
+            }
+        }
+
+        return {
+            top: self.getBoundingClientRect().top + window.scrollY,
+            left: self.getBoundingClientRect().left + window.scrollX,
+            width: self.clientWidth || self.offsetWidth,
+            height: self.clientHeight || self.offsetHeight,
+            scrollTop: self.pageYOffset || self.scrollTop,
+            scrollLeft: self.pageXOffset || self.scrollLeft,
+            boxSizing: parseFloat(calcstyle('boxSizing')),
+            paddingTop: parseFloat(calcstyle('paddingTop')),
+            paddingLeft: parseFloat(calcstyle('paddingLeft')),
+            paddingBottom: parseFloat(calcstyle('paddingBottom')),
+            paddingRight: parseFloat(calcstyle('paddingRight')),
+            marginTop: parseFloat(calcstyle('marginTop')),
+            marginLeft: parseFloat(calcstyle('marginLeft')),
+            marginBottom: parseFloat(calcstyle('marginBottom')),
+            marginRight: parseFloat(calcstyle('marginRight')),
+            borderTop: parseFloat(calcstyle('borderTopWidth')),
+            borderLeft: parseFloat(calcstyle('borderLeftWidth')),
+            borderBottom: parseFloat(calcstyle('borderBottomWidth')),
+            borderRight: parseFloat(calcstyle('borderRightWidth')),
+
+            color: parseFloat(calcstyle('color')),
+            fontSize: parseFloat(calcstyle('fontSize')),
+
+            background: parseFloat(calcstyle('background')),
+
+        };
+    },
+});
+
+Object.defineProperty(HTMLElement.prototype, 'hasClass', {
+    get: function () {
+        return function (arg) {
+            return this && this.nodeType === 1 && this.className.split(/\s+/).indexOf(arg) !== -1;
+        }
+    },
+});
+
+Object.defineProperty(HTMLElement.prototype, 'addClass', {
+    get: function () {
+        return function (arg) {
+            var cls = arg.split(' ');
+            for (var i = 0; i < cls.length; i++) {
+                !this.hasClass(cls[i]) ? this.classList.add(cls[i]) : 0;
+            }
+        }
+    },
+});
+
+Object.defineProperty(HTMLElement.prototype, 'delClass', {
+    get: function () {
+        return function (arg) {
+            var cls = arg.split(' ');
+            for (var i = 0; i < cls.length; i++) {
+                this.hasClass(cls[i]) ? this.classList.remove(cls[i]) : 0;
+            }
+        }
+    },
+});
+
+Object.defineProperty(HTMLElement.prototype, 'trgClass', {
+    get: function () {
+        return function (arg) {
+            var cls = arg.split(' ');
+            for (var i = 0; i < cls.length; i++) {
+                this.hasClass(cls[i]) ? this.classList.remove(cls[i]) : this.classList.add(cls[i]);
+            }
+        }
+    },
+});
+
+Object.defineProperty(HTMLElement.prototype, 'data', {
+    get: function () {
+        var self = this;
+        return function (arg) {
+            return self.getAttribute('data-' + arg);
+        }
+    },
+});
+
+Object.defineProperty(HTMLElement.prototype, 'target', {
+    get: function () {
+        var tgarg = this.data('target');
+        return tgarg ? document.querySelector(tgarg) : null;
+    },
+});
+
+Object.defineProperty(HTMLElement.prototype, 'loadJS', {
+    get: function () {
+        var that = this;
+        return function (arg) {
+            var e = document.createElement('script');
+            e.type = 'text/javascript';
+            e.src = arg;
+            that.appendChild(e);
+            return e;
+        }
+    },
+});
+
+Object.defineProperty(HTMLElement.prototype, 'modal', {
+    get: function () {
+        var that = this;
+        return function (show) {
+            switch (show) {
+                case 'hide':
+                    that.delClass('active');
+                    break;
+                case 'show':
+                    that.addClass('active');
+                    break;
+            }
+        }
+    },
+});
