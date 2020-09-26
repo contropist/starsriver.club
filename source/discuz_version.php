c417d56233ab5fc37b0a8d08cfcd371adcf7e787
starsriver <starsriver@yahoo.com>
2020-09-26 19:56:11 +0800

    交互界面优化 space - friend

--- source/discuz_version.php
+++ source/discuz_version.php
@@ -1,4 +1,10 @@
 <?php
+/********************************************************************
+ * Copyright (c) 2020 All Right Reserved By [StarsRiver]            *
+ *                                                                  *
+ * Author  Zhangyu                                                  *
+ * Email   starsriver@yahoo.com                                     *
+ ********************************************************************/
 
 /**
  *      [Discuz!] (C)2001-2099 Comsenz Inc.
@@ -13,7 +19,7 @@ if(!defined('IN_DISCUZ')) {
 
 if(!defined('DISCUZ_VERSION')) {
 	define('DISCUZ_VERSION', 'X3.4');
-	define('DISCUZ_RELEASE', '20200819 - <a href="https://gitee.com/ComsenzDiscuz/DiscuzX/pulls/503" target="_blank">最新创建 - 544</a>');
+	define('DISCUZ_RELEASE', '20200819 - <a href="https://gitee.com/ComsenzDiscuz/DiscuzX/pulls/503" target="_blank">最新创建 - 566</a>');
 }
 
 ?>
